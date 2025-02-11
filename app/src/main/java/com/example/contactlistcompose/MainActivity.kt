ede61b7ffb81b2111d834ef9f7ba4ea7e7b31f95
CarlaLouise <148821309+CarlaLouise@users.noreply.github.com>
2025-02-11 00:12:28 -0300

    Criação do botao para alternar entre lista e grade. Utilização do isGrid e LazyVerticalGrid. Criação do IconButton

--- app/src/main/java/com/example/contactlistcompose/MainActivity.kt
+++ app/src/main/java/com/example/contactlistcompose/MainActivity.kt
@@ -10,15 +10,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -30,6 +37,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.contactlistcompose.ui.theme.ContactListComposeTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,124 +50,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val angelica = Contato(
-                        name = "Angelica",
-                        telephony = "(+55) 11 953495674",
-                        image = R.drawable.angelica
-                    )
-
-                    val antony = Contato(
-                        name = "Antony",
-                        telephony = "(+55) 21 946783409",
-                        image = R.drawable.antony
-                    )
-
-                    val bruna = Contato(
-                        name = "Bruna",
-                        telephony = "(+55) 85 946783409",
-                        image = R.drawable.bruna
-                    )
-
-                    val eduardo = Contato(
-                        name = "Eduardo",
-                        telephony = "(+55) 85 947783539",
-                        image = R.drawable.eduardo
-                    )
-
-
-                    val fernanda = Contato(
-                        name = "Fernanda",
-                        telephony = "(+55) 87 927672481",
-                        image = R.drawable.fernanda
-                    )
-
-                    val iury = Contato(
-                        name = "Iury",
-                        telephony = "(+55) 21 967452217",
-                        image = R.drawable.iury
-                    )
-
-                    val katia = Contato(
-                        name = "Katia",
-                        telephony = "(+55) 82 945401489",
-                        image = R.drawable.katia
-                    )
-
-                    val leila = Contato(
-                        name = "Leila",
-                        telephony = "(+55) 81 933365137",
-                        image = R.drawable.leila
-                    )
-
-                    val lucas = Contato(
-                        name = "Lucas",
-                        telephony = "(+55) 81 951375137",
-                        image = R.drawable.lucas
-                    )
-
-                    val maria = Contato(
-                        name = "Maria",
-                        telephony = "(+55) 21 983267145",
-                        image = R.drawable.maria
-                    )
-
-                    val nisan = Contato(
-                        name = "Nisan",
-                        telephony = "(+55) 11 924568129",
-                        image = R.drawable.nisan
-                    )
-
-                    val olivia = Contato(
-                        name = "Olivia",
-                        telephony = "(+55) 21 952196520",
-                        image = R.drawable.olivia
-                    )
-
-                    val patricia = Contato(
-                        name = "Patricia",
-                        telephony = "(+55) 71 942136702",
-                        image = R.drawable.patricia
-                    )
-
-                    val samara = Contato(
-                        name = "Samara",
-                        telephony = "(+55) 84 984287204",
-                        image = R.drawable.samara
-                    )
-
-                    val silvio = Contato(
-                        name = "Silvio",
-                        telephony = "(+55) 87 937778899",
-                        image = R.drawable.silvio
-                    )
-
-                    val vicente = Contato(
-                        name = "Vicente",
-                        telephony = "(+55) 81 987329974",
-                        image = R.drawable.vicente
-                    )
 
                     val contato = listOf(
-                        angelica, antony, bruna, eduardo, fernanda,
-                        iury, katia, leila, lucas, maria, nisan,
-                        olivia, patricia, samara, silvio, vicente
+                        Contato("Angelica","(+55) 11 953495674", R.drawable.angelica),
+                        Contato("Antony","(+55) 21 946783409", R.drawable.antony),
+                        Contato("Bruna","(+55) 85 946783409", R.drawable.bruna),
+                        Contato("Eduardo","(+55) 85 947783539", R.drawable.eduardo),
+                        Contato("Fernanda","(+55) 87 927672481", R.drawable.fernanda),
+                        Contato("Iury","(+55) 21 967452217", R.drawable.iury),
+                        Contato("Katia","(+55) 82 945401489", R.drawable.katia),
+                        Contato("Leila","(+55) 81 933365137", R.drawable.leila),
+                        Contato("Lucas","(+55) 81 951375137", R.drawable.lucas),
+                        Contato("Maria","(+55) 21 983267145", R.drawable.maria),
+                        Contato("Nisan","(+55) 11 924568129", R.drawable.nisan),
+                        Contato("Olivia","(+55) 21 952196520", R.drawable.olivia),
+                        Contato( "Patricia","(+55) 71 942136702", R.drawable.patricia),
+                        Contato("Samara","(+55) 84 984287204", R.drawable.samara),
+                        Contato("Silvio","(+55) 87 937778899", R.drawable.silvio),
+                        Contato("Vicente","(+55) 81 987329974", R.drawable.vicente)
                     )
 
-                    LazyColumn {
-                        items(contato) { contato ->
-                            Contactcard(
-                                contact = contato,
-                                onClick = {
-                                    println("test" + contato.name)
-                                }
-                            )
-                        }
-                    }
+                    ContactListWithHeader(contato)
+
+                   }
                 }
             }
         }
     }
-}
 
 @Composable
 fun Contactcard(
@@ -195,6 +113,49 @@ fun Contactcard(
     }
 }
 
+@Composable
+fun ContactListWithHeader(contato: List<Contato>) {
+    var isGrid by remember { mutableStateOf(false) }
+
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Contact List",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.weight(1f)
+            )
+            IconButton(onClick = { isGrid = !isGrid}) {
+                Icon(painter = painterResource(if (isGrid)R.drawable.ic_list else R.drawable.ic_grid),
+                    contentDescription = "Alternar Exibição"
+                )
+            }
+        }
+        if (isGrid){
+            LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = Modifier.padding(8.dp)) {
+                items(contato.size) { index: Int ->
+                    Contactcard(contact = contato[index],
+                        onClick = { println("Clicked: ${contato[index].name} ") }
+                    )
+                }
+            }
+        }else{
+            LazyColumn {
+                items(contato){ contato ->
+                    Contactcard(
+                        contact = contato,
+                        onClick = { println("Clicked: ${contato.name}") }
+                    )
+                }
+            }
+        }
+    }
+}
 data class Contato(
     val name: String,
     val telephony: String,
